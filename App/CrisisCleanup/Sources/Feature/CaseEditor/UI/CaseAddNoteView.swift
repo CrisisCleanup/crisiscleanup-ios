9a8bcc89635614a24834199709205d1f08cfab80
aj1342 <anthonyj.aguil@gmail.com>
2023-07-20 22:34:38 -0400

    Implemented UI for CreateAddNoteView

--- App/CrisisCleanup/Sources/Feature/CaseEditor/UI/CaseAddNoteView.swift
+++ App/CrisisCleanup/Sources/Feature/CaseEditor/UI/CaseAddNoteView.swift
@@ -5,8 +5,42 @@ struct CaseAddNoteView: View {
     @Environment(\.translator) var t: KeyAssetTranslator
 
     @ObservedObject var viewModel: CaseAddNoteViewModel
+    @State var note: String = ""
 
     var body: some View {
-        Text("Add note under construction")
+        VStack {
+            HStack {
+                Text(t.t("caseView.add_note"))
+                    .font(.title)
+            }
+
+            TextEditor(text: $note)
+                .frame(height: appTheme.rowItemHeight*5)
+                .textFieldBorder()
+                .padding()
+                .tint(.black)
+
+            HStack {
+                Spacer()
+                Button {
+                    dismiss.callAsFunction()
+                } label: {
+                    Text(t.t("actions.cancel"))
+                }
+                .tint(.black)
+                .padding(.trailing)
+
+                Button {
+                    // TODO: handle note addition
+                } label: {
+                    Text(t.t("actions.add"))
+                }
+                .tint(.black)
+                .padding(.horizontal)
+
+            }
+            .padding()
+            Spacer()
+        }
     }
 }
