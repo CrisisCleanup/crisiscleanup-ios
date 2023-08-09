41e4b246b7990e2c3301153c9fe68e6cb0a4ebda
aj1342 <anthonyj.aguil@gmail.com>
2023-08-09 13:20:42 -0400

    Implanted placeholders for CaseHistoryView

--- App/CrisisCleanup/Sources/Feature/CaseEditor/UI/CaseHistoryView.swift
+++ App/CrisisCleanup/Sources/Feature/CaseEditor/UI/CaseHistoryView.swift
@@ -7,6 +7,76 @@ struct CaseHistoryView: View {
     @ObservedObject var viewModel: CaseHistoryViewModel
 
     var body: some View {
-        Text("Case history under construction")
+        ScrollView {
+            VStack(alignment: .leading) {
+                Text(t.t("caseHistory.do_not_share_contact_warning"))
+                    .padding([.horizontal, .top])
+                    .bold()
+                Text(t.t("caseHistory.do_not_share_contact_explanation"))
+                    .padding([.horizontal, .bottom])
+
+                Text("~~No History")
+                    .padding(.horizontal)
+
+                HistoryCard()
+                HistoryCard()
+                HistoryCard()
+                Spacer()
+            }
+        }
+    }
+}
+
+
+struct HistoryCard: View {
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("First Last")
+                        .bold() // TODO: change to app font
+                        .foregroundColor(Color.gray)
+                    Text("Some Org Inc")
+                        .font(.caption) // TODO: change to app font
+                        .foregroundColor(Color.gray)
+                }
+
+                Spacer()
+
+                VStack(alignment: .trailing) {
+                    let phoneText = "123-456-7890"
+                    Text(phoneText)
+                        .font(.caption)
+                        .customLink(urlString: "tel:\(phoneText)")
+
+                    let email = "firstlast@email.com"
+                    Text(email)
+                        .font(.caption)
+                        .customLink(urlString: email)
+                }
+            }
+            .padding()
+
+            HistoryAction()
+                .padding()
+            HistoryAction()
+                .padding()
+        }
+        .cardContainerPadded()
+    }
+}
+
+struct HistoryAction: View {
+    var body: some View {
+        VStack(alignment: .leading ) {
+            Text("Viewed case C67 in Medium Flood")
+            HStack {
+                Text("1 Day ago • Some, Location")
+                    .font(.caption)
+                    .foregroundColor(Color.gray)
+                Spacer()
+            }
+        }
     }
 }
