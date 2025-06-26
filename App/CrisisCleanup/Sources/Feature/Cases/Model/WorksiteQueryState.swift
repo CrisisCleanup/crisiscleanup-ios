f8136e8fe0676d335c2495dbb975500dbb817a74
hue <developer@quickytools.com>
2025-06-26 17:05:01 -0400

    Remove unused properties

--- App/CrisisCleanup/Sources/Feature/Cases/Model/WorksiteQueryState.swift
+++ App/CrisisCleanup/Sources/Feature/Cases/Model/WorksiteQueryState.swift
@@ -11,11 +11,9 @@ let CoordinateBoundsDefault = CoordinateBounds(
 // sourcery: copyBuilder
 struct WorksiteQueryState {
     let incidentId: Int64
-    let q: String
     let zoom: Double
     let coordinateBounds: CoordinateBounds
     let isTableView: Bool
-    let isZoomInteractive: Bool
     let tableViewSort: WorksiteSortBy
     let filters: CasesFilter
     let hasLocationPermission: Bool
@@ -23,11 +21,9 @@ struct WorksiteQueryState {
 
 let WorksiteQueryStateDefault = WorksiteQueryState(
     incidentId: EmptyIncident.id,
-    q: "",
     zoom: 0,
     coordinateBounds: CoordinateBoundsDefault,
     isTableView: false,
-    isZoomInteractive: false,
     tableViewSort: .none,
     filters: CasesFilter(),
     hasLocationPermission: false
