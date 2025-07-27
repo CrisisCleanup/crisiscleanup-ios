30a3b891c884c7b3a8cb1ad23c22f723ec82fcd3
hue <developer@quickytools.com>
2025-07-27 10:50:41 -0400

    Apply map marker offset where grouping is dense

--- App/CrisisCleanup/Sources/Feature/Cases/Map/CasesMapMarkerManager.swift
+++ App/CrisisCleanup/Sources/Feature/Cases/Map/CasesMapMarkerManager.swift
@@ -1,10 +1,11 @@
 import Foundation
+import MapKit
 
 class CasesMapMarkerManager {
     private let worksitesRepository: WorksitesRepository
     private let locationManager: LocationManager
 
-    let zeroOffset = (0.0, 0.0)
+    let zeroOffset = CGPoint.zero
 
     init(
         worksitesRepository: WorksitesRepository,
@@ -144,13 +145,13 @@ class CasesMapMarkerManager {
     }
 
     private let denseMarkCountThreshold = 15
-    private let denseMarkZoomThreshold = 14.0
+    private let denseMarkZoomThreshold = CasesConstant.MAP_MARKERS_ZOOM_LEVEL + 4.0
     private let denseDegreeThreshold = 0.0001
-    private let denseScreenOffsetScale = 0.6
+    private let denseScreenOffsetScale = 64.0
     func denseMarkerOffsets(
         _ marks: [WorksiteMapMark],
         _ mapZoom: Double
-    ) throws -> [(Double, Double)] {
+    ) throws -> [CGPoint] {
         if marks.count > denseMarkCountThreshold ||
             mapZoom < denseMarkZoomThreshold
         {
@@ -200,9 +201,9 @@ class CasesMapMarkerManager {
                     var offsetDir = .pi * 0.5
                     let deltaDirDegrees = 2.0 * .pi / count
                     $0.enumerated().forEach { (index, _) in
-                        markOffsets[index] = (
+                        markOffsets[index] = CGPointMake(
                             offsetScale * cos(offsetDir),
-                            offsetScale * sin(offsetDir)
+                            offsetScale * sin(offsetDir),
                         )
                         offsetDir += deltaDirDegrees
                     }
